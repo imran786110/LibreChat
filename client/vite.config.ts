f853b658b86db2f62bd1ae505de26e1295ccd4b5
Imran Hussain <imranhussain.cse@gmail.com>
2026-02-08 10:21:01 +0100

    fix build error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -102,7 +102,7 @@ export default defineConfig(({ command }) => ({
   build: {
     sourcemap: process.env.NODE_ENV === 'development',
     outDir: './dist',
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       preserveEntrySignatures: 'strict',
       output: {
